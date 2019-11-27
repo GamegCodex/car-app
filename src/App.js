773168e56e63f85dc7fb8836e5a5bd085c1d6a05
Javakoodari <51989715+Javakoodari@users.noreply.github.com>
2019-11-27 15:09:56 +0200

    Muutoksia

--- src/App.js
+++ src/App.js
@@ -3,24 +3,47 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    const meta = {
+        charset: 'utf-8'
+    };
+    const carList = [
+        {
+            mark: 'Audi',
+            model: 'A6'
+        },
+        {
+            mark: 'BMW',
+            model: '320i'
+        },
+        {
+            mark: 'Citroen',
+            model: 'CX'
+        }
+    ];
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+                <a
+                    className="App-link"
+                    href="http://www.gameg.fi"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Marko duunissa
         </a>
-      </header>
-    </div>
-  );
+                <ul>
+                    {
+                        ['Audi', 'BMW', 'Citroen'].map(function (item) {
+                            return <li key={item}>{item}</li>
+                        })}
+                </ul>
+            </header>
+        </div>
+    );
 }
 
 export default App;
