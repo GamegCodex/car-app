e081848724e4dbef628b4d0c9063d4badeeefd08
Javakoodari <51989715+Javakoodari@users.noreply.github.com>
2019-11-28 10:14:47 +0200

    Marko juttu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-    const meta = {
-        charset: 'utf-8'
-    };
+class Car {
+    constructor(mark) {
+        this.mark = mark;
+    }
+
+    present() {
+        return 'Auton merkki ' + this;
+    }
+}
+
+class App extends Component {
+    // final list carList
+
     const carList = [
         {
             mark: 'Audi',
@@ -20,30 +29,35 @@ function App() {
             model: 'CX'
         }
     ];
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
+
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
         </p>
-                <a
-                    className="App-link"
-                    href="http://www.gameg.fi"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Marko duunissa
+                    <a
+                        className="App-link"
+                        href="http://www.gameg.fi"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Marko duunissa
         </a>
-                <ul>
-                    {
-                        ['Audi', 'BMW', 'Citroen'].map(function (item) {
-                            return <li key={item}>{item}</li>
-                        })}
-                </ul>
-            </header>
-        </div>
-    );
+                    <ul>
+                        {
+                            [<a href href="https:www.audi.com">Audi</a>, <a href href="https:www.bmw.com">BMW</a>, <a href href="https:www.citroen.com">Citroen</a>].map(function (item) {
+                                return <li key={item}>{item}</li>
+                            })}
+                    </ul>
+                    {mycar.present()}
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
